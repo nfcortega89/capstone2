f98284251d084022ec0b333e4007ee5e42525c1c
Niccolo Ortega <nfcortega89@gmail.com>
2017-10-02 17:25:32 -0700

    jump to results

--- public/js/app.js
+++ public/js/app.js
@@ -1,92 +1,91 @@
 var state = {};
 
 function main() {
-
   // food search bar
   var getResults = function() {
-    $('#food').on('submit', function(e) {
-
+    $("#food").on("submit", function(e) {
       e.preventDefault();
+      $("#food").val("");
+      $("body").scrollTo("#results");
+
       // get the input from recipe
-      var dishes = $('#dishes').val();
+      var dishes = $("#dishes").val();
       // console.log('APP.JS', dishes)
       // set input value back to blank
-      $('#dishes').val(null);
+      $("#dishes").val(null);
       // query strings
-      var url = '/recipes?food=' + dishes;
-      $('.jumbotron').show();
-      $('footer').show();
+      var url = "/recipes?food=" + dishes;
+      $(".jumbotron").show();
+      $("footer").show();
       // make request to server
       $.ajax({
-        type: 'GET',
-        dataType: 'json',
+        type: "GET",
+        dataType: "json",
         url: url
-      }).done(function(data) {
-        $('h3').show()
-        var dish = data.matches;
-        console.log(dish);
-        for (var i = 0; i < dish.length; i++) {
-          var result = showResults(dish[i])
-          $('#results').append(result);
-        }
-
-      }).fail(function(err) {
-        console.error('(50)Error: ' + err);
       })
-    }
-    // $("#food").val('')
-    $("body").scrollTo('#results')
-  )
-  }
+        .done(function(data) {
+          $("h3").show();
+          var dish = data.matches;
+          console.log(dish);
+          for (var i = 0; i < dish.length; i++) {
+            var result = showResults(dish[i]);
+            $("#results").append(result);
+          }
+        })
+        .fail(function(err) {
+          console.error("(50)Error: " + err);
+        });
+    });
+  };
   // clones elements for each recipe
   var showResults = function(recipes) {
     // use two classes to pick out elements
-    var results = $('.template .recipe-results').clone();
+    var results = $(".template .recipe-results").clone();
 
     // we give each recipe it's respectable id
-    results.attr('id', recipes.id);
+    results.attr("id", recipes.id);
 
-    var image = results.find('.recipe-image img');
-    image.attr('src', recipes.imageUrlsBySize["90"]);
+    var image = results.find(".recipe-image img");
+    image.attr("src", recipes.imageUrlsBySize["90"]);
 
-    var recipeNames = results.find('.recipe-name');
+    var recipeNames = results.find(".recipe-name");
     recipeNames.text(recipes.recipeName);
 
     return results;
-  }
+  };
 
   // don't think im setting the url route
   var showDiets = function() {
-    $('.diets li').on('click', 'a', function() {
-      $('.jumbotron').show();
-      var url = '/recipes/' + $(this).attr('id');
+    $(".diets li").on("click", "a", function() {
+      $(".jumbotron").show();
+      var url = "/recipes/" + $(this).attr("id");
       $.ajax({
-        type: 'GET',
-        dataType: 'json',
+        type: "GET",
+        dataType: "json",
         url: url
       }).done(function(data) {
-        $('h3').show()
+        $("h3").show();
         var dish = data.matches;
-        console.log(dish)
+        console.log(dish);
         for (var i = 0; i < dish.length; i++) {
-          var result = showResults(dish[i])
-          console.log($('#results'));
-          $('#results').append(result);
+          var result = showResults(dish[i]);
+          console.log($("#results"));
+          $("#results").append(result);
         }
-      })
-    })
-  }
+      });
+    });
+  };
 
   // gets recipe nutritional facts
   var bindResultClickEvent = function() {
     // bind click event on results and returns detailed information
-    $('#results').on('click', '.recipe-results', function() {
+    $("#results").on("click", ".recipe-results", function() {
       // use 'this' to get attr of id
-      var url = '/recipe/' + $(this).attr('id')
-      $('.selected').show();
+      var url = "/recipe/" + $(this).attr("id");
+      $(".selected").show();
       $.ajax({
-        type: 'GET',
-        dataType: 'json',
+        type: "GET",
+        dataType: "json",
         url: url
       }).done(function(data) {
         console.log(data);
@@ -97,45 +96,45 @@ function main() {
         var ingredients = data.ingredientLines;
         var nutri = [];
         data.nutritionEstimates.forEach(function(attr) {
-          nutri.push(attr)
-        })
+          nutri.push(attr);
+        });
         console.log(nutri);
         for (var i = 0; i < ingredients.length; i++) {
-          str += '<li>' + ingredients[i] + '</li><br>';
+          str += "<li>" + ingredients[i] + "</li><br>";
         }
-        var current = selected(image, recipe, str, time)
-        $('.selected').replaceWith(current);
-      })
-    })
-  }
+        var current = selected(image, recipe, str, time);
+        $(".selected").replaceWith(current);
+      });
+    });
+  };
 
   // picks out which elements will show on right container
   var selected = function(image, recipe, ingredient, time, nutritions) {
     // use two classes to pick out elements
 
-    var results = $('.jumbotron .selected ').clone();
+    var results = $(".jumbotron .selected ").clone();
 
-    var images = results.find('.select img');
-    images.attr('src', image)
+    var images = results.find(".select img");
+    images.attr("src", image);
 
-    var recipeNames = results.find('.recipe');
+    var recipeNames = results.find(".recipe");
     recipeNames.text(recipe);
 
-    var ingredients = results.find('.ingredients');
-    ingredients.html(ingredient)
+    var ingredients = results.find(".ingredients");
+    ingredients.html(ingredient);
 
-    var times = results.find('.time');
-    times.text('Cooking Time: ' + time);
+    var times = results.find(".time");
+    times.text("Cooking Time: " + time);
 
-    var nutrients = results.find('.nutrition');
+    var nutrients = results.find(".nutrition");
 
     return results;
-  }
+  };
 
   // init
   getResults();
   bindResultClickEvent();
   showDiets();
 }
-$(document).on('ready', main);
+$(document).on("ready", main);
 //
